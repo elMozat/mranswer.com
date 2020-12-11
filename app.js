82488309a8920794be4ca59ff56983206b0b68fa
elMozat <mosesaubyn1984@gmail.com>
2020-12-10 20:44:43 -0500

    second commit

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const morgan = require('morgan');
 
 const app = express();
 
+const path = require('path');
+const bodyParser = require('body-parser');
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
