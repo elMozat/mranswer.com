337cc4f4be9b6e96d1360c12464ee17adae6d5b9
elMozat <mosesaubyn1984@gmail.com>
2020-12-10 23:20:38 -0500

    really working now

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
+const userRoutes = require('./routes/userRoutes');
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(`${__dirname}/public`));
 
-app.get('/', (req, res) => {
-  res.send('Hello Mr Answer');
-});
+app.use(userRoutes);
 
 module.exports = app;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.route('/', (req, res) => {
-  res.render('/admin/index');
+router.get('/index', (req, res) => {
+  res.render('./admin/index');
 });
 
 module.exports = router;
